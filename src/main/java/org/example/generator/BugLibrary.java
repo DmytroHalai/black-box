61d62ec346dbb80d735c96bf13bc070d2800adfb
DmytroHalai <dimagalai04@gmail.com>
2025-11-03 16:00:58 +0200

    Add bug methods for threeInRow logic in BugLibrary

--- src/main/java/org/example/generator/BugLibrary.java
+++ src/main/java/org/example/generator/BugLibrary.java
@@ -1,4 +1,36 @@
 package org.example.generator;
 
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.BooleanLiteralExpr;
+import com.github.javaparser.ast.expr.UnaryExpr;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ReturnStmt;
+
 public class BugLibrary {
+    // ========== Bugs for threeInRow ==========
+
+    public static void bugThreeInRowAlwaysTrue(MethodDeclaration m) {
+        m.setBody(new BlockStmt().addStatement(new ReturnStmt("true")));
+    }
+
+
+    public static void bugThreeInRowAlwaysFalse(MethodDeclaration m) {
+        m.setBody(new BlockStmt().addStatement(new ReturnStmt("false")));
+    }
+
+
+    public static void bugThreeInRowInvertCondition(MethodDeclaration m) {
+        m.findAll(ReturnStmt.class).forEach(ret -> ret.setExpression(new UnaryExpr(ret.getExpression()
+                .orElse(new BooleanLiteralExpr(true)), UnaryExpr.Operator.LOGICAL_COMPLEMENT)));
+    }
+
+
+    public static void bugThreeInRowCompareOnlyTwo(MethodDeclaration m) {
+        m.setBody(new BlockStmt().addStatement(new ReturnStmt("board[i] == board[j]")));
+    }
+
+
+    public static void bugThreeInRowSkipEmptyCheck(MethodDeclaration m) {
+        m.setBody(new BlockStmt().addStatement(new ReturnStmt("board[i] == board[j] && board[j] == board[k]")));
+    }
 }
