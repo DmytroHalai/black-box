8804eaf15da34e1cff6348ece61d5c2744bb4d06
DmytroHalai <dimagalai04@gmail.com>
2025-11-06 12:35:33 +0200

    Remove unused imports and clean up EngineTest class

--- src/test/java/game_tests/EngineTest.java
+++ src/test/java/game_tests/EngineTest.java
@@ -3,11 +3,6 @@ package game_tests;
 import org.example.app_logic.api.GameEngine;
 import org.example.test_runner.GameEngineFactory;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EngineTest {
     private GameEngine engine;
