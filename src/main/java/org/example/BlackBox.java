0bf86c6f450bb25bd6a39eb5c9fa3f680babc086
DimaHalai <dimagalai04@gmail.com>
2025-11-08 12:26:08 +0200

    Add Spring Boot to project

---
+++ src/main/java/org/example/BlackBox.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackBox {
+    public static void main(String[] args) {
+        SpringApplication.run(BlackBox.class, args);
+    }
+}
