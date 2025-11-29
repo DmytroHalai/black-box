af6f12b2d37462612d260cb7c6a4e92e3c36ddf9
DimaHalai <dimagalai04@gmail.com>
2025-11-29 21:44:20 +0000

    Remove unused import

--- src/main/java/org/example/web/service/StudentService.java
+++ src/main/java/org/example/web/service/StudentService.java
@@ -5,7 +5,6 @@ import org.example.web.model.Student;
 import org.example.web.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
